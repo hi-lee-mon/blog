65e248e8b66c08518d7097c7b44e075ecec65345
hi-lee-mon <shunmel.09@gmail.com>
2025-07-12 12:47:17 +0900

    ✨ feat: フォント設定を更新し、レイアウトの言語を日本語に変更しました。`globals.css`にRootLayout用のフォント変数を追加し、`layout.tsx`で使用するフォントを変更しました。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_JP, Geist_Mono, M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const mPlusRounded1c = M_PLUS_Rounded_1c({
+  variable: "--font-m-plus-rounded-1c",
+  subsets: ["latin", "latin-ext"],
+  weight: ["300", "400", "500", "700"],
 });
 
+// コードフォント
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -25,9 +27,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mPlusRounded1c.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
         {children}
