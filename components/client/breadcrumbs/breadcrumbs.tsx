f732db8da20302ada5b7c46b0e7ce3bc9f0750cf
hi-lee-mon <shunmel.09@gmail.com>
2025-08-13 09:14:32 +0900

    ✨ feat: Breadcrumbsコンポーネントにホームリンクを追加。スタイルを調整。最後のパス要素の表示方法を改善しました。

--- components/client/breadcrumbs/breadcrumbs.tsx
+++ components/client/breadcrumbs/breadcrumbs.tsx
@@ -1,26 +1,34 @@
 "use client";
+import { Home } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Fragment } from "react";
 
 export default function Breadcrumbs() {
   const pathname = usePathname();
   const paths = pathname.split("/").slice(1);
   return (
-    <div>
+    <div className="flex gap-1 items-center">
+      <Link href="/" className="text-blue-500 hover:underline">
+        <Home />
+      </Link>
       {paths.map((path, index) => {
-        const pathname = paths.slice(0, index + 1).join("/");
-        if (index === paths.length - 1) {
+        const isLastItem = index === paths.length - 1;
+        if (isLastItem) {
           // 最後はスラッシュ不要でリンクにしない
           return <span key={path}>{path}</span>;
         }
+        const pathname = paths.slice(0, index + 1).join("/");
         return (
-          <Link
-            key={path}
-            href={`/${pathname}`}
-            className="text-blue-500 hover:underline"
-          >
-            {path}/
-          </Link>
+          <Fragment key={path}>
+            <Link
+              href={`/${pathname}`}
+              className="text-blue-500 hover:underline"
+            >
+              {path}
+            </Link>
+            <span>/</span>
+          </Fragment>
         );
       })}
     </div>
