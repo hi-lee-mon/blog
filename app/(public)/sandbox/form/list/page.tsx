723021d2e8bbc7251ad30bc1255c1ff8877d953e
hi-lee-mon <shunmel.09@gmail.com>
2025-08-31 23:01:37 +0900

    ✨ feat: Loadingコンポーネントを追加し、ページコンポーネントから<Suspense>を削除して非同期処理を簡素化しました。

--- app/(public)/sandbox/form/list/page.tsx
+++ app/(public)/sandbox/form/list/page.tsx
@@ -1,24 +1,17 @@
-import { Suspense } from "react";
 import { getContacts } from "../contact";
 
 export default async function Page() {
+  const contacts = await getContacts();
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {(async () => {
-        const contacts = await getContacts();
-        return (
-          <ul className="mt-4 space-y-4 border p-4">
-            {contacts.map((contact) => (
-              <li key={contact.id}>
-                <p>名前: {contact.name}</p>
-                <p>メールアドレス: {contact.email}</p>
-                <p>メッセージ: {contact.message}</p>
-                <p>作成日時: {contact.createdAt.toISOString()}</p>
-              </li>
-            ))}
-          </ul>
-        );
-      })()}
-    </Suspense>
+    <ul className="mt-4 space-y-4 border p-4">
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <p>名前: {contact.name}</p>
+          <p>メールアドレス: {contact.email}</p>
+          <p>メッセージ: {contact.message}</p>
+          <p>作成日時: {contact.createdAt.toISOString()}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
